d346591b1b1a948c0f151b58e357ca5914ce0a46
Oleg Plotnikov <plotnikov@multivi.ru>
2016-04-15 12:26:37 +0500

    init2

--- src/entry.js
+++ src/entry.js
@@ -3,4 +3,8 @@ var commonjsDep = require('./commonjs-dep')
 var ts = require('file.ts')
 var template = require('../static/template.html')
 
+// Пример использования webpack.ProvidePlugin
+// globalVar содержит строку 'string from ts' из file.ts
+alert(globalVar)
+
 alert('Welcome:' + amdDep + commonjsDep, ts)
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    entry: "./src/entry",
-    output: {
-        path: path.resolve("./out"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        root: path.resolve('./src'),
-        extensions: ["", ".webpack.js", ".web.js", ".js"]
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader'
-            },
-            {
-                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-                loader: 'file?name=[path][name].[extlf]'
-            },
-            {
-                test: /\.html$/,
-                loader: "html"
-            }
-        ]
-    },
-    ts: {
-            compilerOptions: {
-            target: 'ES5'
-        }
-    }
+	entry: "./src/entry",
+	output: {
+		path: path.resolve("./out"),
+		filename: "bundle.js"
+	},
+	resolve: {
+		root: path.resolve('./src'),
+		extensions: ["", ".webpack.js", ".web.js", ".js"]
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader'
+			},
+			{
+				test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+				loader: 'file?name=[path][name].[extlf]'
+			},
+			{
+				test: /\.html$/,
+				loader: "html"
+			}
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			globalVar: 'file.ts'
+		})
+	],
+	ts: {
+		compilerOptions: {
+			target: 'ES5'
+		}
+	}
 };
